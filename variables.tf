a83d6700c6eb9c59e7018ac79b22777550cebd79
= <=>
2022-08-26 16:15:40 +0530

    terra7

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
   default     =  "terra"
-  //public_key = "terra.pem"
-}
+ 
 
 variable "instance_type" {
   description = "instance type for ec2"
